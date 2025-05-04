864db170aa7819dbec283ab6144d5201295f44f2
ziyu <ziyu@ziyudeMacBook-Pro.local>
2025-05-05 07:54:46 +0800

    Update config

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -11,3 +11,5 @@ lean_lib LoVe {
 }
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"
+-- require std from git "https://github.com/leanprover/std4" @ "v4.19.0-rc3"
+-- require std from git "https://github.com/leanprover/std4" @ "f523fcb75db2b472cda7d94d6caa5d745b1a0c26"
